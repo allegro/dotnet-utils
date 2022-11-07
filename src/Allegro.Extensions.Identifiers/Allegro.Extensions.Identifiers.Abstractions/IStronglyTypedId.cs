fb0b315a65cb73e14e5d82f5b5d3564b7f5d0459
Pawel Piwowarczyk <pawel.piwowarczyk@allegropay.pl>
2022-11-07 13:23:34 +0100

    review summary

--- src/Allegro.Extensions.Identifiers/Allegro.Extensions.Identifiers.Abstractions/IStronglyTypedId.cs
+++ src/Allegro.Extensions.Identifiers/Allegro.Extensions.Identifiers.Abstractions/IStronglyTypedId.cs
@@ -1,7 +1,7 @@
 namespace Allegro.Extensions.Identifiers.Abstractions;
 
 /// <summary>
-/// Marker interface. Indicates if class should be treated as strongly typed identifier.
+/// Marker interface. Indicates if type should be treated as a strongly typed identifier.
 /// Meziantou.Framework.StronglyTypedId that is recommended as a code generator implements this interface by default  and provides common JSON serializers.
 /// </summary>
 public interface IStronglyTypedId<out T>
