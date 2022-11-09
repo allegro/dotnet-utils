d62f9c7c4960d6ee912f7a9a12e24e114c249207
Pawel Piwowarczyk <pawel.piwowarczyk@allegropay.pl>
2022-11-09 10:52:00 +0100

    fix missing xml comments update nugets

--- src/Allegro.Extensions.Financials/Allegro.Extensions.Financials/ValueObjects/Money.cs
+++ src/Allegro.Extensions.Financials/Allegro.Extensions.Financials/ValueObjects/Money.cs
@@ -1,10 +1,19 @@
 namespace Allegro.Extensions.Financials.ValueObjects;
 
+/// <summary>
+/// Value object to store money related data in standardize way
+/// </summary>
 public partial record Money(
     decimal Amount,
     Currency Currency = Currency.PLN)
 {
+    /// <summary>
+    /// Creates money with value 0 and default currency (PLN)
+    /// </summary>
     public static Money Zero => new(0);
 
+    /// <summary>
+    /// Defualt string representation of money
+    /// </summary>
     public override string ToString() => $"{Amount} {Currency}";
 }
