e5578044b64b7a1af30947f7689abc07e8f8b82b
Jakub Dropia <jakub.dropia@allegropay.pl>
2022-11-15 18:51:52 +0100

    some todos

--- src/Allegro.Extensions.Cqrs/Allegro.Extensions.Cqrs.FluentValidations/FluentCommandValidator.cs
+++ src/Allegro.Extensions.Cqrs/Allegro.Extensions.Cqrs.FluentValidations/FluentCommandValidator.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Allegro.Extensions.Cqrs.FluentValidations;
 
+//TODO: add validator for Data Annotations as separate package
+//TODO: we can add option or mention in readme that validation on asp .net pipeline can be disabled: https://www.talkingdotnet.com/disable-automatic-model-state-validation-in-asp-net-core-2-1/
 internal class FluentCommandValidator<T> : ICommandValidator<T>
     where T : ICommand
 {
@@ -18,6 +20,7 @@ internal class FluentCommandValidator<T> : ICommandValidator<T>
     {
         var requestValidators = _serviceProvider.GetServices<IValidator<T>>();
 
+        //TODO: what about validation context?
         var failures = (await Task.WhenAll(requestValidators.Select(v => v.ValidateAsync(command))))
            .SelectMany(result => result.Errors)
            .Where(f => f != null)
