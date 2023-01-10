993a7d6ece6bdd221ffe34798f13399b461ff1a6
Rafal Schmidt <rafal.schmidt@allegropay.pl>
2023-01-10 11:49:35 +0100

    feat: remove strongly typed reference

--- src/Allegro.Extensions.Identifiers/Allegro.Extensions.Identifiers.Abstractions/IStronglyTypedId.cs
+++ src/Allegro.Extensions.Identifiers/Allegro.Extensions.Identifiers.Abstractions/IStronglyTypedId.cs
@@ -2,7 +2,7 @@ namespace Allegro.Extensions.Identifiers.Abstractions;
 
 /// <summary>
 /// Marker interface. Indicates if type should be treated as a strongly typed identifier.
-/// Meziantou.Framework.StronglyTypedId that is recommended as a code generator implements this interface by default  and provides common JSON serializers.
+/// Meziantou.Framework.StronglyTypedId that is recommended as a code generator implements this interface by default and provides common JSON serializers.
 /// </summary>
 public interface IStronglyTypedId<out T>
 {
