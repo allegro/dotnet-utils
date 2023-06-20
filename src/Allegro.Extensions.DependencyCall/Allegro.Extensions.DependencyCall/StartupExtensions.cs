ea6642118ff9236b98332c98805bdceec92d7e53
Pawel Piwowarczyk <pawel.piwowarczyk@allegropay.pl>
2023-06-20 12:53:01 +0200

    refactor: attach allegro sdk and

--- src/Allegro.Extensions.DependencyCall/Allegro.Extensions.DependencyCall/StartupExtensions.cs
+++ src/Allegro.Extensions.DependencyCall/Allegro.Extensions.DependencyCall/StartupExtensions.cs
@@ -19,9 +19,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
-using FluentValidation;
+
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Allegro.Extensions.DependencyCall;
 
@@ -37,7 +36,6 @@ public static class StartupExtensions
         this IServiceCollection services)
         where TOptions : class
     {
-
         return services;
     }
 }
