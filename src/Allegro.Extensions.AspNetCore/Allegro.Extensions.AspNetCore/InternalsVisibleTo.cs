f20bbe8b029b7781e82f64b5b29cbaf99fa9be74
Amadeusz Sadowski <amadeusz.sadowski@allegro.com>
2025-07-16 12:17:31 +0200

    refactor: move InternalsVisibleTo into csproj

--- src/Allegro.Extensions.AspNetCore/Allegro.Extensions.AspNetCore/InternalsVisibleTo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Allegro.Extensions.AspNetCore.Tests.Unit")]
--- src/Allegro.Extensions.Cqrs/Allegro.Extensions.Cqrs/InternalsVisibleTo.cs
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) PlaceholderCompany. All rights reserved.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Allegro.Extensions.Cqrs.Tests.Unit")]
